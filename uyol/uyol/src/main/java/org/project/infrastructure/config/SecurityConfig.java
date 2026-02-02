cc2af899e5767a2f9f39d8e9ed85a1b1e2bb8387
zakirashirli <117630695+zakirashirli@users.noreply.github.com>
2026-02-02 14:11:11 +0400

    fixed

--- uyol/uyol/src/main/java/org/project/infrastructure/config/SecurityConfig.java
+++ uyol/uyol/src/main/java/org/project/infrastructure/config/SecurityConfig.java
@@ -1,28 +1,28 @@
-//package org.project.infrastructure.config;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.web.SecurityFilterChain;
-//
-//@Configuration
-//public class SecurityConfig {
-//
-//    @Bean
-//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-//        http
-//                .csrf(csrf -> csrf.disable())
-//                .authorizeHttpRequests(auth -> auth
-//                        .requestMatchers(
-//                                "/swagger-ui/**",
-//                                "/v3/api-docs/**",
-//                                "/swagger-ui.html"
-//                        ).permitAll()
-//                        .anyRequest().permitAll()
-//                )
-//                .httpBasic(httpBasic -> httpBasic.disable())
-//                .formLogin(form -> form.disable());
-//
-//        return http.build();
-//    }
-//}
+package org.project.infrastructure.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(
+                                "/swagger-ui/**",
+                                "/v3/api-docs/**",
+                                "/swagger-ui.html"
+                        ).permitAll()
+                        .anyRequest().permitAll()
+                )
+                .httpBasic(httpBasic -> httpBasic.disable())
+                .formLogin(form -> form.disable());
+
+        return http.build();
+    }
+}
